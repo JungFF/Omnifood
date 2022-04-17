8dad554425ec19d3b2ce8c8a999d65065c541855
JungFF <wangjung80@gmail.com>
2022-04-17 19:40:52 -0400

    build the testimonials section

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict'
+const gallery = document.querySelector('.gallery');
+for (let i = 1; i <= 12; i++) {
+    const html = ` <figure class="gallery-item">
+<img src="./img/gallery/gallery-${i}.jpg" alt="Photo of beautifully arranged food">
+</figure>`
+    gallery.insertAdjacentHTML('afterbegin', html);
+}
